682b5d703be6412c4b431058f856408e4b34206d
cleison.ferreirademelo <cleison.ferreirademelo@fiserv.com>
2021-04-08 10:54:40 +0100

    Added more tests cases to invite

--- tests/invites-router.test.js
+++ tests/invites-router.test.js
@@ -2,19 +2,9 @@ import "regenerator-runtime/runtime";
 import request from "supertest";
 const app = require("../app").default;
 let id = "";
+let url = "";
 describe("Invites API", () => {
-    it("Should all invites", async () => {
-        const res = await request(app).get("/invites");
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    url: "http://test.com/xpofsfisaoifaljk",
-                }),
-            ])
-        );
-    }),
-        it("Should create a new invite", async () => {
+        it("Should create a new invite with URL", async () => {
             const res = await request(app).post("/invites").send({
                 url: "http://test.com/fsa894651fafsoiuw",
                 workspace_id: 2,
@@ -26,12 +16,35 @@ describe("Invites API", () => {
                 "http://test.com/fsa894651fafsoiuw"
             );
         }),
+        it("Should all invites", async () => {
+            const res = await request(app).get("/invites");
+            expect(res.statusCode).toEqual(200);
+            expect(res.body).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        url: "http://test.com/fsa894651fafsoiuw",
+                    }),
+                ])
+            );
+        }),  
+        it("Should create a new invite without URL", async () => {
+            const res = await request(app).post("/invites").send({
+                workspace_id: 2,
+            });
+            id = res.body.id;
+            url = res.body.url;
+            expect(res.statusCode).toEqual(201);
+            expect(res.body).toHaveProperty(
+                "url",
+                url
+            );
+        }),              
         it("Should show an invites", async () => {
             const res = await request(app).get("/invites/" + id);
             expect(res.statusCode).toEqual(200);
             expect(res.body).toHaveProperty(
                 "url",
-                "http://test.com/fsa894651fafsoiuw"
+                url
             );
         }),
         it("Should update an invites", async () => {
